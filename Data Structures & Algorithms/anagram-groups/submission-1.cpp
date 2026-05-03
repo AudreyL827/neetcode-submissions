9a9fa839df504d94361ee2f0e92ca816597dbbdf
Audrey Li <127917991+AudreyL827@users.noreply.github.com>
2026-05-03 14:17:42 -0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            groups[key].push_back(strs[i]);
+        }
+
+        vector<vector<string>> results;
+        for (auto x : groups) {
+            results.push_back(x.second);
+        }
+
+        return results;
+    }
+};
