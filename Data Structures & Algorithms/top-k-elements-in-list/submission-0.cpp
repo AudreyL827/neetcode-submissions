67c0e3d50cff495128ffc16abe159862793e555e
Audrey Li <127917991+AudreyL827@users.noreply.github.com>
2026-05-03 14:43:25 -0600

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for (int i = 0; i < nums.size(); i++) {
+            counts[nums[i]]++;
+        }
+
+        vector<pair<int, int>> freq(counts.begin(), counts.end());
+        sort(freq.begin(), freq.end(), []( auto& a, auto& b) {
+            return a.second > b.second;
+            });
+            
+            vector<int> result;
+            for (int i = 0; i < k; i++) {
+                result.push_back(freq[i].first);
+            } 
+
+            return result;  
+    }
+};
